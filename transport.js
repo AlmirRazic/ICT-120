eb60e95aea328b33deeef447305e1958c990ffc9
AlmirRazic <Almir.Razic@cpnv.ch>
2019-11-07 12:15:52 +0100

    v0.9.3
    ajout d'une grande partie des fonctions

--- transport.js
+++ transport.js
@@ -1,3 +1,11 @@
 function changeBgColor(obj, colorName) {
   obj.style.backgroundColor = colorName;
 }
+function aficher(x) {
+  x.style.height = "200px";
+  x.style.width = "200px";
+}
+function normalImg(x) {
+  x.style.height = "0px";
+  x.style.width = "0px";
+}
