ced5c9d33a86ee4b1b6e6db76801bf28c205495c
AlmirRazic <Almir.Razic@cpnv.ch>
2019-11-07 16:51:40 +0100

    v0.9.6
    début du contact

--- transport.js
+++ transport.js
@@ -2,8 +2,8 @@ function changeBgColor(obj, colorName) {
   obj.style.backgroundColor = colorName;
 }
 function aficher(x) {
-  x.style.height = "350px";
-  x.style.width = "350px";
+  x.style.height = "400px";
+  x.style.width = "500px";
   x.style.zIndex = "20";
 }
 function normalImg(x) {
