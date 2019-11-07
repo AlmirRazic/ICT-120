91ee641d3f359646920e177eae02c5cf86e345d5
AlmirRazic <Almir.Razic@cpnv.ch>
2019-11-07 15:22:53 +0100

    ajout de fonction java
    aja

--- transport.js
+++ transport.js
@@ -2,10 +2,12 @@ function changeBgColor(obj, colorName) {
   obj.style.backgroundColor = colorName;
 }
 function aficher(x) {
-  x.style.height = "200px";
-  x.style.width = "200px";
+  x.style.height = "350px";
+  x.style.width = "350px";
+  x.style.zIndex = "20";
 }
 function normalImg(x) {
   x.style.height = "0px";
   x.style.width = "0px";
+  x.style.zIndex = "-1";
 }
